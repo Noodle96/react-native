72e310d5ce143504cd38bcdc43dc4a31c6198658
Noodle96 <jorgealfredomarino2@gmail.com>
2023-11-06 17:43:05 -0500

    Add React Navigation

--- library-app/start.sh
+++ library-app/start.sh
@@ -1,6 +1,14 @@
 1.- Create App
     npx create-expo-app@latest
     cd nameProject
-    nom start
+    npm start
 2.- Clerk
     npm install @clerk/clerk-expo
+3.- React Navigation
+    npm install @react-navigation/native
+    npx expo install react-native-screens react-native-safe-area-context
+    Navigators/stack
+    npm install @react-navigation/stack
+    npx expo install react-native-gesture-handler
+    Navigators/BottomTabs
+    npm install @react-navigation/bottom-tabs
