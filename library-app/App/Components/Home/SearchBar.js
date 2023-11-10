cdae109e3838c17f39878b665dad3a03ead5a710
Noodle96 <jorgealfredomarino2@gmail.com>
2023-11-10 16:39:48 -0500

    add useState para searchInput y add useRef para limpiar el textInput

--- library-app/App/Components/Home/SearchBar.js
+++ library-app/App/Components/Home/SearchBar.js
@@ -1,22 +1,30 @@
 import { View, Text, TextInput, StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { Ionicons } from '@expo/vector-icons';
 import Colors from '../../../assets/Shared/Colors';
 
-export default function SearchBar() {
-  return (
-	<View style={{marginTop:15,}}>
-		<View style={styles.search}>
-			<Ionicons name="search-outline" size={24} color={Colors.PRIMARY} />
-			<TextInput
-				placeholder='Buscar'
-				onChangeText={(value)=>console.log(value)}
-				//con esto evitamos el desborde
-				style={{ flex: 1, marginLeft: 10, width: '80%' }}
-			/>
+export default function SearchBar({setSearchText}) {
+	const [searchInput, setSearchInput] = useState("");
+	const inputRef = useRef(null);
+	const handleOnSubmitEditing = () => {
+		setSearchText(searchInput);
+		setSearchInput("");
+		inputRef.current.clear();
+	}
+	return (
+		<View style={{marginTop:15,}}>
+			<View style={styles.search}>
+				<Ionicons name="search-outline" size={24} color={Colors.PRIMARY} />
+				<TextInput
+					ref={inputRef} // Guarda la referencia al TextInput
+					placeholder='Buscar'
+					style={{ flex: 1, marginLeft: 10, width: '80%' }} //con esto evitamos el desborde
+					onChangeText={(value)=>setSearchInput(value)}
+					onSubmitEditing={handleOnSubmitEditing}
+				/>
+			</View>
 		</View>
-	</View>
-  )
+	)
 }
 
 const styles = StyleSheet.create({
--- library-app/App/Screens/Home.js
+++ library-app/App/Screens/Home.js
@@ -1,15 +1,19 @@
 import { View, Text, Button, StyleSheet } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { useAuth } from '@clerk/clerk-expo';
 import Header from '../Components/Home/Header';
 import SearchBar from '../Components/Home/SearchBar';
 
 export default function Home() {
 	const { isLoaded,signOut } = useAuth();
+	const [searchText, setSearchText] = useState("");
 	return (
 		<View style={styles.view}>
 			<Header />
-			<SearchBar />
+			<SearchBar
+				setSearchText = {setSearchText}
+			/>
+			<Text>Se esta buscando: {searchText}</Text>
 			{/* <Text>Home</Text> */}
 			{/* <Button title='SignOut' onPress={()=>{signOut();}} ></Button> */}
 		</View>
