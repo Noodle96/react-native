556e51ba610072c45521c19dfceece7708d137c2
Noodle96 <jorgealfredomarino2@gmail.com>
2023-11-10 17:52:02 -0500

    Add Slider component and add FlatList inside this

---
+++ library-app/App/Components/Home/Slider.js
@@ -0,0 +1,42 @@
+import { View, Text, StyleSheet, FlatList, Image, Dimensions } from 'react-native'
+import React from 'react'
+
+export default function Slider() {
+	const sliderList = [
+		{
+			id: 1,
+			name: 'slider 01',
+			imageUrl: 'https://img.freepik.com/foto-gratis/abundante-coleccion-libros-antiguos-estantes-madera-generados-ia_188544-29660.jpg?w=1380&t=st=1699655794~exp=1699656394~hmac=09cdce1b23a05a841b63cc4ff032d753a7682d20e13a2b1ab83468c5f144b473',
+		},
+		{
+			id: 2,
+			name: 'slider 02',
+			imageUrl: 'https://img.freepik.com/foto-gratis/retrato-joven-que-estudia-literatura-biblioteca-generado-ia_188544-28137.jpg?w=1380&t=st=1699655943~exp=1699656543~hmac=6ea6631c8523404b8feabd01179ae04a923900a4e26769c6d79fd6263fabddcd',
+		},
+	];
+	return (
+		<View style={styles.container}>
+			<FlatList 
+				data={sliderList}
+				horizontal={true}
+				renderItem={({item})=>(
+					<Image 
+						source={{uri:item.imageUrl}}
+						style={styles.image}
+					/>
+				)}
+			/>
+		</View>
+	)
+}
+const styles = StyleSheet.create({
+	container:{
+		marginTop:15,
+	},
+	image:{
+		width:Dimensions.get('screen').width,
+		height:170,
+		borderRadius:10,
+		margin:.5,
+	}
+});
--- library-app/App/Screens/Home.js
+++ library-app/App/Screens/Home.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import { useAuth } from '@clerk/clerk-expo';
 import Header from '../Components/Home/Header';
 import SearchBar from '../Components/Home/SearchBar';
+import Slider from '../Components/Home/Slider';
 
 export default function Home() {
 	const { isLoaded,signOut } = useAuth();
@@ -13,7 +14,11 @@ export default function Home() {
 			<SearchBar
 				setSearchText = {setSearchText}
 			/>
-			<Text>Se esta buscando: {searchText}</Text>
+			{/* <Text>Se esta buscando: {searchText}</Text> */}
+			<Slider />
+
+
+
 			{/* <Text>Home</Text> */}
 			{/* <Button title='SignOut' onPress={()=>{signOut();}} ></Button> */}
 		</View>
